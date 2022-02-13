cc91e1b76b20622bf21dd319f7609dd4d4c86f2e
CHIkovil <chikovil3488@gmail.com>
2022-02-14 01:06:35 +0300

    Remove .DS_Store from everywhere

---
+++ CoreData/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  
+//
+//  Created by Nikolas on 15.01.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
